b91c451c40c1fb3177f9800a5f2641915bdf7e65
Manuel Barzi <manuelbarzi@gmail.com>
2025-07-29 17:39:41 +0200

    add baby steps #1

---
+++ staff/manuel-barzi/playground/baby-steps/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const calc_1 = require("./calc");
+console.log("Hello, world!");
+console.log((0, calc_1.add)(2, 3)); // Outputs: 5
+console.log((0, calc_1.sub)(5, 2)); // Outputs: 3
+console.log((0, calc_1.mul)(4, 2)); // Outputs: 8
+console.log((0, calc_1.div)(8, 2)); // Outputs: 4
+//# sourceMappingURL=index.js.map
---
+++ staff/manuel-barzi/playground/baby-steps/index.ts
@@ -0,0 +1,9 @@
+import { add, sub, mul, div } from "./calc";
+
+
+console.log("Hello, world!");
+
+console.log(add(2, 3)); // Outputs: 5
+console.log(sub(5, 2)); // Outputs: 3
+console.log(mul(4, 2)); // Outputs: 8
+console.log(div(8, 2)); // Outputs: 4
