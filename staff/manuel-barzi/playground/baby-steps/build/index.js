c3f90454405969ffc4a254b02d1c5349928807f0
Manuel Barzi <manuelbarzi@gmail.com>
2025-07-29 18:06:38 +0200

    add objects demos #1

--- staff/manuel-barzi/playground/baby-steps/build/index.js
+++ staff/manuel-barzi/playground/baby-steps/build/index.js
@@ -6,4 +6,22 @@ console.log((0, calc_1.add)(2, 3)); // Outputs: 5
 console.log((0, calc_1.sub)(5, 2)); // Outputs: 3
 console.log((0, calc_1.mul)(4, 2)); // Outputs: 8
 console.log((0, calc_1.div)(8, 2)); // Outputs: 4
+let vane = { name: "Vanessa", age: 30, city: "New York" };
+let rares = { name: "Rares", age: 25, city: "Bucharest" };
+let mario = { name: "Mario", age: 28, city: "Rome" };
+let berni = { name: "Berni", age: 22, city: "Berlin" };
+class Worker {
+    constructor(name, age, city, country) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.country = country;
+    }
+    work() {
+        console.log(`${this.name} is working.`);
+    }
+}
+let alice = new Worker("Alice", 32, "London", "UK");
+alice.work(); // Outputs: Alice is working.
+console.log(alice instanceof Worker); // Outputs: true
 //# sourceMappingURL=index.js.map
--- staff/manuel-barzi/playground/baby-steps/index.ts
+++ staff/manuel-barzi/playground/baby-steps/index.ts
@@ -7,3 +7,62 @@ console.log(add(2, 3)); // Outputs: 5
 console.log(sub(5, 2)); // Outputs: 3
 console.log(mul(4, 2)); // Outputs: 8
 console.log(div(8, 2)); // Outputs: 4
+
+
+/*
+let vane:{ name: string; age: number; city: string; };
+vane = { name: "Vanessa", age: 30, city: "New York" };
+*/
+
+/*
+let vane = { name: "Vanessa", age: 30, city: "New York" };
+vane = { name: "Vanessa", age: 30, city: "New York" };
+
+let rares:typeof vane;
+rares = { name: "Rares", age: 25, city: "Bucharest" };
+
+type Person = typeof vane;
+
+let mario: Person;
+mario = { name: "Mario", age: 28, city: "Rome" };
+
+let berni: Person;
+berni = { name: "Berni", age: 22, city: "Berlin" };
+*/
+
+
+// type Person = { name: string; age: number; city: string; }
+interface Person { name: string; age: number; city: string; country?: string; }
+
+let vane: Person = { name: "Vanessa", age: 30, city: "New York" };
+let rares: Person = { name: "Rares", age: 25, city: "Bucharest" };
+let mario: Person = { name: "Mario", age: 28, city: "Rome" };
+let berni: Person = { name: "Berni", age: 22, city: "Berlin" };
+
+//type IWorkder = Person & { work(): void; };
+interface IWorkder extends Person {
+    work(): void;
+}
+
+class Worker implements IWorkder {
+  name: string;
+  age: number;
+  city: string;
+  country?: string;
+
+  constructor(name: string, age: number, city: string, country?: string) {
+    this.name = name;
+    this.age = age;
+    this.city = city;
+    this.country = country;
+  }
+
+  work() {
+    console.log(`${this.name} is working.`);
+  }
+}
+
+let alice = new Worker("Alice", 32, "London", "UK");
+alice.work(); // Outputs: Alice is working.
+
+console.log(alice instanceof Worker); // Outputs: true
