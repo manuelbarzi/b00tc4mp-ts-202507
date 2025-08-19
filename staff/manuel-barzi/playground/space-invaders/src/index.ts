5f395ec8f879d8c165dcad92a1ef03c0a3e36eda
Manuel Barzi <manuelbarzi@gmail.com>
2025-08-19 18:08:29 +0200

    implement check-collision logic in space-invaders #1

--- staff/manuel-barzi/playground/space-invaders/src/index.ts
+++ staff/manuel-barzi/playground/space-invaders/src/index.ts
@@ -15,7 +15,7 @@ const constants = {
         dimensions: { width: 30, height: 30 }
     },
     enemy: {
-        dimensions: { width: 40, height: 20 }
+        dimensions: { width: 40, height: 40 }
     },
     bullet: {
         dimensions: { width: 5, height: 10 }
@@ -60,4 +60,32 @@ Logic
 - update lives
 - update level
 - reset game
-*/ 
+*/
+
+function checkCollision() {
+    const playerHalfWidth = constants.player.dimensions.width / 2;
+    const playerHalfHeight = constants.player.dimensions.height / 2;
+    const enemyHalfWidth = constants.enemy.dimensions.width / 2;
+    const enemyHalfHeight = constants.enemy.dimensions.height / 2;
+
+    const playerVertices = {
+        topLeft: { x: state.player.position.x - playerHalfWidth, y: state.player.position.y + playerHalfHeight },
+        topRight: { x: state.player.position.x + playerHalfWidth, y: state.player.position.y + playerHalfHeight },
+        bottomRight: { x: state.player.position.x + playerHalfWidth, y: state.player.position.y - playerHalfHeight },
+        bottomLeft: { x: state.player.position.x - playerHalfWidth, y: state.player.position.y - playerHalfHeight }
+    }
+
+    return state.enemies.some(enemy => {
+        const enemyVertices = {
+            topLeft: { x: enemy.position.x - enemyHalfWidth, y: enemy.position.y + enemyHalfHeight },
+            topRight: { x: enemy.position.x + enemyHalfWidth, y: enemy.position.y + enemyHalfHeight },
+            bottomRight: { x: enemy.position.x + enemyHalfWidth, y: enemy.position.y - enemyHalfHeight },
+            bottomLeft: { x: enemy.position.x - enemyHalfWidth, y: enemy.position.y - enemyHalfHeight }
+        }
+
+        return playerVertices.topLeft.x <= enemyVertices.bottomRight.x &&
+            playerVertices.bottomRight.x >= enemyVertices.topLeft.x &&
+            playerVertices.topLeft.y >= enemyVertices.bottomRight.y &&
+            playerVertices.bottomRight.y <= enemyVertices.topLeft.y
+    })
+}
